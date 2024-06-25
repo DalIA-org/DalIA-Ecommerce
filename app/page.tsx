88c84b8ccc29614344ba89597ffca873435a890f
daniel-dubon-dev <danieldubonxd@gmail.com>
2024-06-25 11:45:39 -0600

    test2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import Text from "@/src/components/Text";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Button>Click me</Button>
+        <Text/>
         <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
