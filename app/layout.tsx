80fbda7516c50fecd3246af11d69fac4618ebb33
benjamin-rivas-dev <alexis.rivas@goes.gob.sv>
2024-07-05 14:20:04 -0600

    beginning the header and hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css"
 import { Inter as FontSans } from "next/font/google"
 
 import { cn } from "@/lib/utils"
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -22,7 +24,9 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
+        <Header/>
        {children}
+       <Footer/>
       </body>
     </html>
   )
